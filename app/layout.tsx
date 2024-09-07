402e01bd7c681749ceef6de41f816d1f46020684
Timodercoder <timo.schafarik@gmail.com>
2024-09-07 22:33:10 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
+
+import 'react-datepicker/dist/react-datepicker.css'
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "CT Meets",
+  description: "AI Supercharged Meetings",
+  icons:{
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider appearance={{layout:{logoImageUrl: '/icons/yoom-logo.svg',},variables: {colorText: '#fff', colorPrimary: '#0E78F9', colorBackground: '#1c1f2e', colorInputBackground: '#252a41', colorInputText: '#fff' }}}>
+        <body
+          className={`${geistSans.variable} ${geistSans.className} bg-dark-2 ${geistMono.variable} antialiased`}
+        >
+          {children}
+          <Toaster />
+        </body>
+      </ClerkProvider>
+      
+    </html>
+  );
+}
